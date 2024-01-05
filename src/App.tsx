813492f359f8b2d1ccb6f0d954cd46d4938ac013
Davi Silva <davi.psilva@midway.com.br>
2024-01-05 17:02:26 -0300

    finished header

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export const App = () => (
   <HelmetProvider>
-    <Helmet titleTemplate="%s | pizza.shop" />
-    <Toaster richColors duration={3000} />
-    <RouterProvider router={router} />
+    <ThemeProvider storageKey="pizzashot-theme" defaultTheme="dark">
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors duration={3000} />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </HelmetProvider>
 )
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -5,10 +5,10 @@ const imageBackground =
   'https://images.unsplash.com/photo-1571997478779-2adcbbe9ab2f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 export const AuthLayout = () => (
-  <div className="grid min-h-screen grid-cols-2">
+  <div className="grid min-h-screen grid-cols-2 antialiased">
     <div
       style={{ backgroundImage: `url(${imageBackground})` }}
-      className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted bg-cover bg-center p-10 text-muted-foreground opacity-90"
+      className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted bg-cover bg-center bg-no-repeat p-10 text-muted-foreground opacity-90"
     >
       <div className="flex items-center gap-3 text-center text-lg text-white">
         <Pizza className="h-5 w-5" />
