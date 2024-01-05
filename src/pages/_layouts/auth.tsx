f7c0295a1a1be899d37ab9f053f625946d7a2732
Davi Silva <davi.psilva@midway.com.br>
2024-01-05 16:06:32 -0300

    adjust AuthLayout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,27 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
-export const AuthLayout = () => {
-  return (
-    <div>
-      <h1>Auth</h1>
+const imageBackground =
+  'https://images.unsplash.com/photo-1571997478779-2adcbbe9ab2f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
-      <div>
-        <Outlet />
+export const AuthLayout = () => (
+  <div className="grid min-h-screen grid-cols-2">
+    <div
+      style={{ backgroundImage: `url(${imageBackground})` }}
+      className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted bg-cover bg-center p-10 text-muted-foreground opacity-90"
+    >
+      <div className="flex items-center gap-3 text-center text-lg text-white">
+        <Pizza className="h-5 w-5" />
+        <span className="font-semibold">pizza.shop</span>
       </div>
+
+      <footer className="text-sm text-white">
+        Painel do parceiro &copy; pizza.shop {new Date().getFullYear()}
+      </footer>
     </div>
-  )
-}
+
+    <div className="relative flex flex-col items-center justify-center">
+      <Outlet />
+    </div>
+  </div>
+)
