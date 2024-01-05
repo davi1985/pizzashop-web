599184513f35a81b790ae16a0ba6bda75202d007
Davi Silva <davi.psilva@midway.com.br>
2024-01-05 16:07:28 -0300

    add toater provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes'
 
 export const App = () => (
   <HelmetProvider>
     <Helmet titleTemplate="%s | pizza.shop" />
+    <Toaster richColors duration={3000} />
     <RouterProvider router={router} />
   </HelmetProvider>
 )
